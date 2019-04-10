291ea9542242d388eac375528d92692483a75540
Aluno <aluno@imac10.local>
2019-04-10 12:25:58 +0100

    Initial Commit

---
+++ appnicial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appnicial
+//
+//  Created by Aluno on 10/04/2019.
+//  Copyright © 2019 ipvc.estg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
