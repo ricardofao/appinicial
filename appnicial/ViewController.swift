31fcd90b4634749d0aea3bcdf30498ce11874897
Aluno <aluno@imac10.local>
2019-05-06 19:44:18 +0100

    novo commit1

--- appnicial/ViewController.swift
+++ appnicial/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: properties
+    @IBOutlet weak var txt1: UITextField!
+    @IBOutlet weak var lbl1: UILabel!
+    
+    //MARK: actions
+    @IBAction func clickButton1(_ sender: UIButton) {
+        lbl1.text = "Escreva na caixa de texto."
+    }
+    
+    //MARK: ciclovida
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSLog(NSLocalizedString("mainipvc.message", comment: ""))
+        
+        txt1.delegate = self as UITextFieldDelegate
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: UITextFieldDelegate
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        lbl1.text = txt1.text
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
